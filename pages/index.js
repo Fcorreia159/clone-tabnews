209853a983a06cb9a5c96325ea2d3b1e5fcafa85
Fcorreia159 <37090336+Fcorreia159@users.noreply.github.com>
2023-11-30 20:27:25 +0000

    "adicionando scripts `lint:check` e `lint:fix `

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Testando Deploy</h1>
+function Home() {
+  return <h1>Testando Deploy</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
+
+function teste2() {
+  console.log("teste");
+}
